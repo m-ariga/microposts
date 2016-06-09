6fdf81542ac094a80f1cd3b8c137a0cb5af3c11b
arico93 <mizuki-ariga@dac.co.jp>
2016-06-09 07:56:44 +0000

    create edit, update, edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  
+  def edit
+    @user = User.find(params[:id])
+    if (current_user!= @user)
+      redirect_to root_path
+    end
+  end
+    
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    flash[:success] = "Your Profile has been updated"
+    else
+      render 'edit'
+    end
+  end
+      
   def show 
    @user = User.find(params[:id])
   end 
@@ -15,12 +34,18 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
-  end
-
+  end  
+    
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
   end
-end
+  
+  def edit_params
+    params.require(:user).permit(:name, :email, :location, :profile)
+  end
+  
+  
+end  
