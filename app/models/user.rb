808462306e881e01a84e9e9456824b3e87140aca
arico93 <mizuki-ariga@dac.co.jp>
2016-06-10 08:23:22 +0000

    use bootstrap for new and edit form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-#  validates :location, presence: true, length: { maximum: 50 }
-#  validates :profile, presence: true, length: {maximum: 50 }
+validates :location, presence: true, length: { maximum: 50 }
+validates :profile, presence: true, length: {maximum: 100 }
     has_secure_password
 end
