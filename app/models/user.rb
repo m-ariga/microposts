774ea63bac09753a31cbf3875dfbedccef66b02f
arico93 <mizuki-ariga@dac.co.jp>
2016-06-13 05:00:02 +0000

    validate location and profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-validates :location, presence: true, length: { maximum: 50 }
-validates :profile, presence: true, length: {maximum: 100 }
+validates :location, absence: true, on: :create
+validates :location, allow_blank: true, length: {minimum: 2, maximum: 20 }, on: :update
+validates :profile, absence: true, on: :create
+validates :profile, allow_blank: true, length: {minimum: 2, maximum: 100 }, on: :update
+
     has_secure_password
 end
